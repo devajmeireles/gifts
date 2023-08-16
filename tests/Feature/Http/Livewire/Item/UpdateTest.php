40f294f1238120a18bb6b208acc46b35adb9761e
AJ Meireles <alvaro.meireles@live.com>
2023-08-15 23:55:54 -0300

    wip

--- tests/Feature/Http/Livewire/Item/UpdateTest.php
+++ tests/Feature/Http/Livewire/Item/UpdateTest.php
@@ -1,23 +1,23 @@
 <?php
 
-use App\Http\Livewire\Item\Index;
-use App\Http\Livewire\Item\Update;
+use App\Http\Livewire\Item\{Index, Update};
 use App\Models\Item;
+
 use function Pest\Livewire\livewire;
 
 beforeEach(fn () => createTestUser());
 
-it('can load item', function () {
+it('can update item', function () {
     $item = Item::factory()
         ->forCategory()
         ->create();
 
-    $name = fake()->word();
+    $name        = fake()->word();
     $description = fake()->sentence();
-    $category = $item->category_id;
-    $quantity = fake()->numberBetween(1, 100);
-    $reference = fake()->url();
-    $activated = fake()->boolean();
+    $category    = $item->category_id;
+    $quantity    = fake()->numberBetween(1, 100);
+    $reference   = fake()->url();
+    $activated   = fake()->boolean();
 
     livewire(Update::class)
         ->call('load', $item)
@@ -32,5 +32,12 @@ it('can load item', function () {
         ->assertSuccessful()
         ->assertEmittedUp('item::index::refresh');
 
-    $update->assertPayloadSet('item', $item->toArray());
+    $item->refresh();
+
+    expect($item->name)
+        ->toBe($name)
+        ->and($item->description)
+        ->toBe($description)
+        ->and($item->category_id)
+        ->toBe($category);
 });

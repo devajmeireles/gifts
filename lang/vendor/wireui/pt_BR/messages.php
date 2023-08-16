3958a62077d422723cc7ccf5b17e0922b98e66e5
AJ Meireles <alvaro.meireles@live.com>
2023-08-16 00:04:21 -0300

    wip

---
+++ lang/vendor/wireui/pt_BR/messages.php
@@ -0,0 +1,18 @@
+
+<?php
+
+return [
+    'selectTime'    => 'Selecione a Hora',
+    'empty_options' => 'Nenhum Resultado Encontrado',
+    'searchHere'    => 'Busca Aqui',
+    'datePicker'    => [
+        'months'    => "['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']",
+        'days'      => "['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab']",
+        'tomorrow'  => 'Amanhã',
+        'today'     => 'Hoje',
+        'yesterday' => 'Ontem',
+    ],
+    'errors' => [
+        'title' => 'Foram encontrados {errors} erros de validação',
+    ],
+];

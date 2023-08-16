a39dc7e2e0d6a8b798d66f6152692202db90c056
AJ Meireles <alvaro.meireles@live.com>
2023-08-15 23:34:49 -0300

    wip

---
+++ tests/Feature/Http/Livewire/Item/DeleteTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Livewire\Item\Delete;
+use App\Http\Livewire\Item\Index;
+use App\Models\Item;
+
+use function Pest\Laravel\assertModelMissing;
+use function Pest\Livewire\livewire;
+
+it('can delete item', function () {
+    createTestUser();
+
+    $item = Item::factory()
+        ->forCategory()
+        ->create();
+
+    livewire(Delete::class)
+        ->call('load', $item)
+        ->assertDispatchedBrowserEvent('wireui:confirm-notification')
+        ->call('delete')
+        ->assertEmittedUp('item::index::refresh');
+
+    assertModelMissing($item);
+});

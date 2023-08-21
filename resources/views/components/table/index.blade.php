c797119f7fa50fea9c68ebe2cd65b8d84bc256e9
AJ Meireles <alvaro.meireles@live.com>
2023-08-21 19:37:36 -0300

    custom scroll bar

--- resources/views/components/table/index.blade.php
+++ resources/views/components/table/index.blade.php
@@ -1,4 +1,4 @@
-<div class="overflow-auto rounded-lg shadow ring-1 ring-black ring-opacity-5">
+<div class="overflow-auto rounded-lg shadow ring-1 ring-black ring-opacity-5 custom-scrollbar">
     <table class="min-w-full divide-y divide-gray-300">
         {{ $slot }}
     </table>

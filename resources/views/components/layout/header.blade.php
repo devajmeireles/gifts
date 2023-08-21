dc58f3ce36e3f72570ba924b159a69178c799499
AJ Meireles <alvaro.meireles@live.com>
2023-08-21 19:23:57 -0300

    frontend shotcut

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -6,6 +6,9 @@
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
         </svg>
     </button>
+    <x-button.circle sm primary href="{{ route('frontend') }}" target="_blank">
+        <x-heroicon-s-arrow-up-right class="h-6 w-6 text-white" />
+    </x-button.circle>
     <div class="flex flex-1 justify-end gap-x-4 self-stretch lg:gap-x-6">
         <div class="flex items-center gap-2">
             @if (session()->has('impersonate'))

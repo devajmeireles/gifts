0ef4be8934f25a2f34d534b4856408c9a2ad3451
AJ Meireles <alvaro.meireles@live.com>
2023-08-21 20:43:54 -0300

    typing

--- app/Filters/Item/FilterCategoryItem.php
+++ app/Filters/Item/FilterCategoryItem.php
@@ -5,12 +5,13 @@ namespace App\Filters\Item;
 use App\Filters\Traits\ShareableConstructor;
 use Closure;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class FilterCategoryItem
 {
     use ShareableConstructor;
 
-    public function handle(Builder $builder, Closure $next)
+    public function handle(Builder $builder, Closure $next): LengthAwarePaginator
     {
         if (($category = data_get($this->filters, 'category')) !== null) {
             $builder->where('category_id', '=', $category);

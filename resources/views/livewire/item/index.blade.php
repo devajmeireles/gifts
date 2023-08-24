47a7dbb13be79702b8cd8e1301ccad541302153a
AJ Meireles <alvaro.meireles@live.com>
2023-08-23 23:19:36 -0300

    removing unnecessary class="px-4 sm:px-6 lg:px-8" from tables

--- resources/views/livewire/item/index.blade.php
+++ resources/views/livewire/item/index.blade.php
@@ -1,6 +1,6 @@
 <div>
     @php /** @var \App\Models\Item $item */ @endphp
-    <div class="px-4 sm:px-6 lg:px-8">
+    <div>
         <div class="flex items-end justify-end gap-2">
             @if (!($guest = user()->isGuest()))
                 <livewire:item.create />

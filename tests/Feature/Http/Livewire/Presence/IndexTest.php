f0acf9407b3e2949553325a229de79dc0e214f67
AJ Meireles <alvaro.meireles@live.com>
2023-08-23 23:42:28 -0300

    formatting

--- tests/Feature/Http/Livewire/Presence/IndexTest.php
+++ tests/Feature/Http/Livewire/Presence/IndexTest.php
@@ -1,11 +1,5 @@
 <?php
 
-use App\Http\Livewire\Setting\Index;
-use App\Models\Setting;
-
-use function Pest\Laravel\get;
-use function Pest\Livewire\livewire;
-
 beforeEach(fn () => createTestUser());
 
 it('can render component')->todo();
@@ -21,4 +15,3 @@ it('can change quantity')->todo();
 it('can sort')->todo();
 
 it('can filter for empty results')->todo();
-

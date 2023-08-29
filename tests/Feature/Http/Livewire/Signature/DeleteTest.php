69187bad936e3a324e906e2782a3326c205bc9b8
AJ Meireles <alvaro.meireles@live.com>
2023-08-28 22:54:36 -0300

    wip

--- tests/Feature/Http/Livewire/Signature/DeleteTest.php
+++ tests/Feature/Http/Livewire/Signature/DeleteTest.php
@@ -2,8 +2,9 @@
 
 use App\Http\Livewire\Signature\Delete;
 use App\Models\{Item, Signature};
+use App\Services\Settings\Facades\Settings;
 
-use function Pest\Laravel\assertDatabaseEmpty;
+use function Pest\Laravel\{assertDatabaseCount, assertDatabaseEmpty};
 use function Pest\Livewire\livewire;
 
 beforeEach(fn () => createTestUser());
@@ -11,13 +12,17 @@ beforeEach(fn () => createTestUser());
 it('can delete', function () {
     $signature = Signature::factory()
         ->forItem()
+        ->forPresence()
         ->create();
+
+    assertDatabaseCount('presences', 1);
 
     livewire(Delete::class, ['signature' => $signature])
         ->call('delete')
         ->assertHasNoErrors();
 
     assertDatabaseEmpty('signatures');
+    assertDatabaseCount('presences', 1);
 });
 
 it('can delete reactivating item', function () {
@@ -39,4 +44,25 @@ it('can delete reactivating item', function () {
         ->toBeTrue()
         ->and($item->last_signed_at)
         ->toBeNull();
+});
+
+it('can delete and delete presence', function () {
+    Settings::shouldReceive('get')
+        ->with('converter_assinaturas_em_presenca')
+        ->once()
+        ->andReturnTrue();
+
+    $signature = Signature::factory()
+        ->forItem()
+        ->forPresence()
+        ->create();
+
+    assertDatabaseCount('presences', 1);
+
+    livewire(Delete::class, ['signature' => $signature])
+        ->call('delete')
+        ->assertHasNoErrors();
+
+    assertDatabaseEmpty('signatures');
+    assertDatabaseEmpty('presences');
 });

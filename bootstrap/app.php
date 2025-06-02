0cb72e3e0b2813ee3f91f6957009ace722b18d4c
AJ Meireles <alvaro.meireles@live.com>
2025-06-02 20:58:43 -0300

    wip

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,7 +10,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('admin.login'));
+
+        $middleware->trimStrings([
+            'current_password',
+            'password',
+            'password_confirmation',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
